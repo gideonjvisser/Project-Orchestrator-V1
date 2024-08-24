e08d2f57f6a49c352cdc33826d3e4b3fc98751af
gideonjvisser <gjvisser1984@gmail.com>
2024-08-24 22:05:45 +0200

    Graph 1

---
+++ repo.bat
@@ -0,0 +1,6 @@
+@echo off
+
+git add . & echo Added all files.
+git commit -m "Graph 1" & echo Committed changes with message.
+git push origin main & echo Pushed changes to remote repository (origin/main).
+git status & echo Displaying current git status.
